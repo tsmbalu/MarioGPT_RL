20fe513353a00e1f210d1492c02cdea12508a989
Bala <balu.aravi@gmail.com>
2024-09-27 14:34:04 +0100

    Update

--- mario_gpt/usability_test.py
+++ mario_gpt/usability_test.py
@@ -56,6 +56,11 @@ def show_level_images(img1, img2, border_size=5, border_color=(0, 0, 0)):
     root = tk.Tk()
     root.title("Level Images")
 
+    # Bring window to the foreground
+    root.lift()
+    root.attributes("-topmost", True)
+    root.focus_force()1
+
     # Convert the combined image to ImageTk format for Tkinter
     combined_image_tk = ImageTk.PhotoImage(combined_image)
 
